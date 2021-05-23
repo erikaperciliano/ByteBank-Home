b176426f6ecdbaaf78ff9cbd50ff7775ab125a05
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-23 18:21:03 -0300

    add hearder com  a alterização como padrão

--- src/http/index.js
+++ src/http/index.js
@@ -6,6 +6,16 @@ const http = axios.create({
         'Accept': 'application/json',
         'Content': 'application/json'
     }
+});
+
+http.interceptors.request.use(function(config){
+    const token = localStorage.getItem('token')
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, function(err){
+    return Promise.reject(err)
 })
 
 export default http
