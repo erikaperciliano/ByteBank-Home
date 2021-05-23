7464ec2b71a7e16ec40ea0b108993d4ea991ab98
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-23 19:58:06 -0300

    add ações

--- src/provedor.js
+++ src/provedor.js
@@ -1,5 +1,6 @@
 import Vuex from  'vuex';
 import Vue from 'vue';
+import http from '@/http';
 
 Vue.use(Vuex);
 
@@ -7,15 +8,40 @@ const estado = {
     token: null,
     usuario: {}
 }
-
+//mutação precisa estar em caixa alta
 const mutations = {
     DEFINIR_USUARIO_LODAGO(state, { token, usuario }){
         state.usuario = usuario
         state.token = token
+    },
+
+    DESLOGAR_USUARIO(state){
+        state.token = null
+        state.usuario = {}
+    }
+}
+
+const actions = {
+    efetuarLogin({ commit }, usuario){
+        return new Promise((resolve, reject) => {
+            http.post('auth/login', usuario)
+                .then(res => {
+                    commit('DEFINIR_USUARIO_LODAGO', {
+                        token: res.data.access_token,
+                        usuario: res.data.user
+                    })
+                    resolve(res.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                    reject(err)
+                })
+        })
     }
 }
 
 export default new Vuex.Store({
     state: estado,
-    mutations
+    mutations,
+    actions
 })
